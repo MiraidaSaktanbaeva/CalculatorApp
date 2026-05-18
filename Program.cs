c196c0c75900dcff0b06a727229025e5fadf00af
Мираида <miraidasaktanbaeva2006@gmail.com>
2026-05-18 10:54:33 +0600

    Базовый калькулятор

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 0, b = 0;
+
+        while (true)
+        {
+            Console.WriteLine("\nВыберите действие:");
+            Console.WriteLine("1. Ввести A");
+            Console.WriteLine("2. Ввести B");
+            Console.WriteLine("3. +");
+            Console.WriteLine("4. -");
+            Console.WriteLine("5. *");
+            Console.WriteLine("6. /");
+
+            Console.Write("Ваш выбор: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Write("Введите A: ");
+                a = Convert.ToDouble(Console.ReadLine());
+            }
+            else if (choice == "2")
+            {
+                Console.Write("Введите B: ");
+                b = Convert.ToDouble(Console.ReadLine());
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("Результат: " + (a + b));
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Результат: " + (a - b));
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Результат: " + (a * b));
+            }
+            else if (choice == "6")
+            {
+                Console.WriteLine("Результат: " + (a / b));
+            }
+        }
+    }
+}
